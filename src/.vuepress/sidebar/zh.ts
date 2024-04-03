d4f7208aba7132357d3dd5f2c65932ea46c63661
XieXiLin <support@xiexilin.com>
2024-04-03 23:23:59 +0800

    🛠️ fix: wrong

--- src/.vuepress/sidebar/zh.ts
+++ src/.vuepress/sidebar/zh.ts
@@ -6,10 +6,14 @@ export const zhSidebar = sidebar({
       text: "Git",
       icon: "pajamas:gitea",
       children: [
-        { text: "如何注册", icon: "mdi:register", linkL: "docs/git/register" },
-        { text: "隐私条款", icon: "material-symbols-light:private-connectivity-rounded", link: "docs/git/privacy" },
-        { text: "使用条款", icon: "ri:service-fill", link: "docs/git/tos" }
-      ]
-    }
+        { text: "如何注册", icon: "mdi:register", link: "docs/git/register" },
+        {
+          text: "隐私条款",
+          icon: "material-symbols-light:private-connectivity-rounded",
+          link: "docs/git/privacy",
+        },
+        { text: "使用条款", icon: "ri:service-fill", link: "docs/git/tos" },
+      ],
+    },
   ],
 });
